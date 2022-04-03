5a1902dfdeb34360d129f088ea182a7496ecb989
Bablu Mia <bablumiah2000@gmail.com>
2022-04-04 00:08:06 +0600

    contact added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { Accordion } from 'react-bootstrap';
 import './Blog.css'
 
 const Blog = () => {
     return (
         <div className='min-h'>
-            <h2>from blog</h2>
+            <h2 className='text-center my-4'>We Are Here To Answere</h2>
+            <div className="row mx-auto  w-75 px-2 ">
+                <div className="col-11 col-lg-11">
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Accordion Item #1</Accordion.Header>
+                            <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Accordion Item #1</Accordion.Header>
+                            <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>Accordion Item #2</Accordion.Header>
+                            <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                est laborum.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Dashbord/Dashbord.js
+++ src/Components/Dashbord/Dashbord.js
@@ -1,9 +1,121 @@
 import React from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashbord = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
-            <h2 className='text-center'>See Our Management Dashbord</h2>
+        <div className='min-h ms-0 ms-lg-5'>
+            <h2 className='text-center my-4'>See Our Management Dashbord</h2>
+            <div className="row ms-1 ms-lg-5  ps-lg-0 g-5 px-lg-5 px-0 mx-auto justify-content-between">
+                <div className="col-12 col-lg-6">
+                    <h3 className='text-center my-3'>Month Wise Sale</h3>
+                    <LineChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis dataKey="sell" />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    </LineChart>
+                </div>
+                <div className="col-12 col-lg-6">
+                    <h3 className='text-center my-3'>Investment Vs Raveniew</h3>
+                    <AreaChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis dataKey="investment" />
+                        <Tooltip />
+                        <Legend />
+                        <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                        <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                    </AreaChart>
+                </div>
+                <div className="col-12 col-lg-6">
+                    <h3 className='text-center my-3'>Investment Vs Raveniew</h3>
+                    <BarChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis dataKey="investment" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                        <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+                <div className="col-5">
+                    {/* <PieChart width={400} height={400}>
+                        <Pie data="data" dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                        <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                    </PieChart> */}
+                </div>
+            </div>
         </div>
     );
 };
