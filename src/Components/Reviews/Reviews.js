20960108da70f1de7d7781e15c6b947589f7f09a
Bablu Mia <bablumiah2000@gmail.com>
2022-04-04 16:51:43 +0600

    fetch done

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
+
+import useReviews from '../hooks/useReviews';
 import './Reviews.css'
 
 
 const Reviews = () => {
-    useEffect(()=>{
-        fetch("data.json")
-        .then(res => res.json())
-        .then(data => console.log(data))
-    },[])
+    const [reviews , setReviews] = useReviews()
+    console.log(reviews);
     return (
         <div>
             <h1 className='my-4 text-center'>See Our Happy Customar</h1>
+            <div className="container ">
+            <div className="row w-75 mx-auto mt-4">
+                {reviews.map(review =><div className="col-lg-3 mx-4 my-3 bg-danger text-center">
+                    <p>{review.id}</p>
+                </div> )}
+            </div>
+            </div>
         </div>
     );
 };
